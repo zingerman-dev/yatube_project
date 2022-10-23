9348017d78c9141193d1cf0ee8dbfe06ee9b927d
Peter Zingerman <zingerman.dev@gmail.com>
2022-10-23 19:43:19 +0300

    Trying to fix groups pages

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,6 +6,6 @@ def index(request):
     return HttpResponse('Главная страница')
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
-def group_posts(request, pk):
-    return HttpResponse(f'Посты группы {pk}')
+def group_posts(request, slug):
+    return HttpResponse(f'Посты группы {slug}')
 
