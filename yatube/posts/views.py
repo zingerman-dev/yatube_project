b3d40a50f77e8db4eaae4b153dad330fa4b0a31f
Peter Zingerman <zingerman.dev@gmail.com>
2022-10-27 18:30:12 +0300

    Templates added

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template: str = 'posts/index.html'
+    return render(request, template)
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
 def group_posts(request, slug):
