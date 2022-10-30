5dfde90485a8e32d9bd174de9d7abbaeb304e8ae
Peter Zingerman <zingerman.dev@gmail.com>
2022-10-30 21:53:33 +0300

    Templates connected

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('group/<slug:group>/', views.group_posts),
+    #path('group/<slug:group>/', views.group_posts('group')),
+    path('group_list/', views.group_list),
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,9 +5,13 @@ from django.template import loader
 
 def index(request):
     template: str = 'posts/index.html'
-    return render(request, template)
+    return render(request, template, context={'title': 'Это главная страница проекта Yatube'})
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
 def group_posts(request, slug):
     return HttpResponse(f'Посты группы {slug}')
+
+def group_list(request):
+    template: str = 'posts/group_list.html'
+    return render(request, template, context={'title': 'Здесь будет информация о группах проекта Yatube'})
 
